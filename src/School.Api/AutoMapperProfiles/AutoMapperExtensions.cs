62da3443e9726ec3171137c7d375cfd95dd69325
fakhrul.islam <mamun1net@gmail.com>
2019-11-20 17:51:15 +0800

    AutoMapper added

---
+++ src/School.Api/AutoMapperProfiles/AutoMapperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace School.Api.AutoMapperProfiles
+{
+    public static class AutoMapperExtensions
+    {
+        public static IMappingExpression<TSource, TDestination> IgnoreMember<TDestination, TSource, TMember>(
+            this IMappingExpression<TSource, TDestination> mapping, Expression<Func<TDestination, TMember>> destinationMember)
+        {
+            return mapping.ForMember(destinationMember, options => options.Ignore());
+        }
+
+        public static IMappingExpression<TSource, TDestination> MapMember<TDestination, TSource, TMember>(
+            this IMappingExpression<TSource, TDestination> mapping,
+            Expression<Func<TSource, TMember>> sourceMember,
+            Expression<Func<TDestination, TMember>> destinationMember)
+        {
+            return mapping.ForMember(destinationMember, options => options.MapFrom(sourceMember));
+        }
+    }
+}
---
+++ src/School.Api/AutoMapperProfiles/SchoolProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using School.Api.Commands.CreateSchool;
+using School.Api.Commands.UpdateSchool;
+using School.Api.Queries.GetSchoolDetail;
+
+namespace School.Api.AutoMapperProfiles
+{
+    public class SchoolProfile : Profile
+    {
+        public SchoolProfile()
+        {
+            CreateMap<CreateSchoolCommand, Data.Entities.School>()
+                .IgnoreMember(dest => dest.RefId);
+
+            CreateMap<UpdateSchoolCommand, Data.Entities.School>()
+                .IgnoreMember(dest => dest.RefId);
+
+            CreateMap<Data.Entities.School, GetSchoolDetailQueryResponse>(MemberList.Source);
+        }
+    }
+}
